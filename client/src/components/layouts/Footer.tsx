6739f331bd4cb8863feb6dc2c9e8c32cf4a0f877
DAT DOAN <datdoangongzuo@gmail.com>
2024-09-26 22:02:48 +0700

    finished animation for footer..

--- client/src/components/layouts/Footer.tsx
+++ client/src/components/layouts/Footer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./Footer.scss";
 
@@ -8,60 +8,102 @@ import tagname from "../../assets/images/datdoan.png";
 type Props = {};
 
 const Footer = (props: Props) => {
+  const footerRef = useRef<HTMLDivElement>(null);
+  const headerFooterRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (footerRef.current && headerFooterRef.current) {
+        const windowHeight = window.innerHeight;
+        const scrollPosition = window.scrollY;
+        const footerOffsetTop = footerRef.current.offsetTop;
+
+        // Calculate the height based on the scroll position
+        if (scrollPosition + windowHeight >= footerOffsetTop) {
+          const maxScroll = footerOffsetTop - windowHeight;
+          const scrollRatio = Math.min(
+            (scrollPosition + windowHeight - footerOffsetTop) / maxScroll,
+            1
+          );
+          const height = 800 * scrollRatio;
+          headerFooterRef.current.style.height = `${height}px`;
+        } else {
+          headerFooterRef.current.style.height = `0px`;
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="footer">
-      <div className="header-wrapper">
-        <Link to="/">
-          <img src={logo} className="logo_nextin" alt="Logo" />
-        </Link>
-        <div className="author">
-          <span>Design and Development</span>
-          <img src={tagname} alt="" className="tagname" />
+    <div className="footer" ref={footerRef}>
+      <div
+        className="header-footer"
+        ref={headerFooterRef}
+        style={{
+          transition: "height .1s ease",
+        }}
+      ></div>
+      <div className="footer-contain">
+        {/* header wrapper */}
+        <div className="header-wrapper">
+          <Link to="/">
+            <img src={logo} className="logo_nextin" alt="Logo" />
+          </Link>
+          <div className="author">
+            <span>Design and Development</span>
+            <img src={tagname} alt="" className="tagname" />
+          </div>
         </div>
-      </div>
-      {/* main content */}
-      <div className="main-content">
-        <div className="infor">
-          <ul>
-            <li>
-              <a href="mailto:datdoanofficial@gmail.com">
-                datdoanofficial@gmail.com
-              </a>
-            </li>
-            <li>
-              <a href="tel:(+84)916709786">(+84) 916 709 786</a>
-            </li>
-            <li>
-              <p>Ho Chi Minh City - Viet Nam</p>
-            </li>
-          </ul>
+        {/* main content */}
+        <div className="main-content">
+          <div className="infor">
+            <ul>
+              <li>
+                <a href="mailto:datdoanofficial@gmail.com">
+                  datdoanofficial@gmail.com
+                </a>
+              </li>
+              <li>
+                <a href="tel:(+84)916709786">(+84) 916 709 786</a>
+              </li>
+              <li>
+                <p>Ho Chi Minh City - Viet Nam</p>
+              </li>
+            </ul>
+          </div>
+          <div className="links">
+            <ul>
+              <li>
+                <a href="/">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="/">Terms & Conditions</a>
+              </li>
+              <div className="copy-right">
+                &copy; Next In | All rights reserved
+              </div>
+            </ul>
+          </div>
         </div>
-        <div className="links">
-          <ul>
-            <li>
-              <a href="/">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="/">Terms & Conditions</a>
-            </li>
-            <div className="copy-right">
-              &copy; Next In | All rights reserved
-            </div>
-          </ul>
-        </div>
-      </div>
-      {/* send mail */}
-      <div className="send-mail">
-        <div className="title">Be the first to know</div>
-        <p>
-          Get special offers, exclusive product news, and event info straight to
-          your inbox.
-        </p>
-        <div className="mail-form">
-          <input type="text" placeholder="Enter your email" />
-          <button>
-            <span className="ph--arrow-right-bold icon"></span>
-          </button>
+        {/* send mail */}
+        <div className="send-mail">
+          <div className="title">Be the first to know</div>
+          <p>
+            Get special offers, exclusive product news, and event info straight
+            to your inbox.
+          </p>
+          <div className="mail-form">
+            <input type="text" placeholder="Enter your email" />
+            <button>
+              <span className="ph--arrow-right-bold icon"></span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
