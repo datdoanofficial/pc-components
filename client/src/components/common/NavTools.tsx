35e84f15c3eb5c746f87c421e2ed83adda235ad2
DAT DOAN <datdoangongzuo@gmail.com>
2024-10-16 11:20:40 +0700

    finished the card box..

--- client/src/components/common/NavTools.tsx
+++ client/src/components/common/NavTools.tsx
@@ -4,21 +4,63 @@ import { Link } from "react-router-dom";
 import QuantityBtn from "./QuantityBtn";
 
 import product_demo from "../../assets/images/product-details/4070ti-demo.png";
+import product_demo2 from "../../assets/images/product-details/4090-demo.png";
+import product_demo3 from "../../assets/images/product-details/4090msi-demo.png";
+import product_demo4 from "../../assets/images/product-details/4080wf.png";
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>;
 
 type Props = {};
 
 const NavTools = (props: Props) => {
+  const products = [
+    {
+      id: 1,
+      image: product_demo,
+      name: "ZOTAC Gaming GeForce RTX 4070 Ti Trinity OC White Edition",
+      brand: "Gigabyte",
+      guarantee: "36 Months",
+      price: 719.99,
+    },
+    {
+      id: 2,
+      image: product_demo2,
+      name: "ASUS ROG Strix GeForce RTX™ 4090 White OC Ed",
+      brand: "ASUS ROG",
+      guarantee: "36 Months",
+      price: 2099.99,
+    },
+    {
+      id: 3,
+      image: product_demo3,
+      name: "MSI GeForce RTX 4090 GAMING X TRIO 24G",
+      brand: "MSI",
+      guarantee: "36 Months",
+      price: 1899.99,
+    },
+    {
+      id: 4,
+      image: product_demo4,
+      name: "AORUS GeForce RTX™ 4080 16GB XTREME WATERFORCE WB",
+      brand: "AORUS",
+      guarantee: "36 Months",
+      price: 1499.0,
+    },
+  ];
+
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const [quantity, setQuantity] = useState(1);
+  const [quantities, setQuantities] = useState(products.map(() => 1));
   const cartRef = useRef<HTMLDivElement>(null);
 
-  const basePrice = 719.99;
-  const totalPrice = basePrice * quantity;
-
-  const handleQuantityChange = (newQuantity: number) => {
-    setQuantity(newQuantity);
+  const handleQuantityChange = (index: number, newQuantity: number) => {
+    const newQuantities = [...quantities];
+    newQuantities[index] = newQuantity;
+    setQuantities(newQuantities);
   };
+
+  const totalPrice = products.reduce((total, product, index) => {
+    return total + product.price * quantities[index];
+  }, 0);
 
   const toggleCart = () => {
     setIsCartOpen(!isCartOpen);
@@ -34,25 +76,25 @@ const NavTools = (props: Props) => {
     if (isCartOpen) {
       document.addEventListener("mousedown", handleClickOutside);
       document.body.style.overflow = "hidden"; // Disable scrolling
+      document.body.style.paddingRight = "15px"; // Adjust for scrollbar width
     } else {
       document.removeEventListener("mousedown", handleClickOutside);
       document.body.style.overflow = "auto"; // Enable scrolling
+      document.body.style.paddingRight = "0"; // Reset padding
     }
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
       document.body.style.overflow = "auto"; // Ensure scrolling is enabled on cleanup
+      document.body.style.paddingRight = "0"; // Reset padding on cleanup
     };
   }, [isCartOpen]);
 
-  const MAX_PRODUCT_NAME_LENGTH = 50; // Set your desired max length here
-
-  const productName =
-    "ZOTAC Gaming GeForce RTX 4070 Ti Trinity OC White Edition";
-  const truncatedProductName =
-    productName.length > MAX_PRODUCT_NAME_LENGTH
-      ? `${productName.substring(0, MAX_PRODUCT_NAME_LENGTH)}...`
-      : productName;
+  const truncateName = (name: string, maxLength: number) => {
+    return name.length > maxLength
+      ? name.substring(0, maxLength - 3) + "..."
+      : name;
+  };
 
   return (
     <div className="tool-bar">
@@ -62,46 +104,66 @@ const NavTools = (props: Props) => {
         sign in
       </Link>
       <div className={`cart-box ${isCartOpen ? "open" : ""}`} ref={cartRef}>
-        <div className="heading">
-          <div className="title">Shopping Cart</div>
-          <span className="close-btn" onClick={toggleCart}>
-            <span className="line-md--remove"></span>
-          </span>
-        </div>
-        {/* product items */}
-        <div className="product-items">
-          {/* img */}
-          <div className="product-image">
-            <img src={product_demo} alt="" />
+        <div className="cart-content">
+          {/* heading */}
+          <div className="heading">
+            <div className="title">Shopping Cart</div>
+            <span className="close-btn" onClick={toggleCart}>
+              <span className="line-md--remove"></span>
+            </span>
           </div>
-          {/* information */}
-          <div className="product-info">
-            {/* name */}
-            <div className="product-name" title={productName}>
-              {truncatedProductName}
-            </div>
-            {/* details */}
-            <div className="product-details">
-              <div className="brand">
-                <span>Brand:</span> Gigabyte
+          {/* product box */}
+          <div className="product-box">
+            {/* product items */}
+            {products.map((product, index) => (
+              <div key={product.id} className="product-items">
+                {/* img */}
+                <div className="product-image">
+                  <img src={product.image} alt={product.name} />
+                </div>
+                {/* information */}
+                <div className="product-info">
+                  {/* name */}
+                  <div className="product-name" title={product.name}>
+                    {truncateName(product.name, 40)}
+                  </div>
+                  {/* details */}
+                  <div className="product-details">
+                    <div className="brand">
+                      <span>Brand:</span> {product.brand}
+                    </div>
+                    <div className="guarantee">
+                      <span>Guarantee:</span> {product.guarantee}
+                    </div>
+                  </div>
+                </div>
+                <div className="action">
+                  {/* remove btn */}
+                  <div className="remove-toggle">
+                    <span className="fluent--delete-48-regular"></span>
+                  </div>
+                  <div className="quantity-toggle">
+                    <QuantityBtn
+                      showTitle={false}
+                      onQuantityChange={(newQuantity) =>
+                        handleQuantityChange(index, newQuantity)
+                      }
+                    />
+                    <div className="price">
+                      ${(product.price * quantities[index]).toFixed(2)}
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="guarantee">
-                <span>Guarantee:</span> 36 Months
-              </div>
-            </div>
+            ))}
           </div>
-          <div className="action">
-            {/* remove btn */}
-            <div className="remove-toggle">
-              <span className="fluent--delete-48-regular"></span>
-            </div>
-            <div className="quantity-toggle">
-              <QuantityBtn
-                showTitle={false}
-                onQuantityChange={handleQuantityChange}
-              />
+          {/* cart footer */}
+          <div className="cart-footer">
+            <div className="subtotal">
+              <p>Subtotal</p>
               <div className="price">${totalPrice.toFixed(2)}</div>
             </div>
+            <button className="check-out">check out</button>
           </div>
         </div>
       </div>
